8243fcdb18c8a0499e61b92b64e6735de96e6035
wuliqun <liqun@qianyutech.cn>
2023-07-21 18:45:05 +0800

    add doc &request pool

---
+++ utils/request-pool.ts
@@ -0,0 +1,24 @@
+/**
+ * 请求池
+ * @param {Array<()=>Promise<any>>} fns 待处理请求
+ * @param {number} maxCount 最大允许同时存在的请求
+ */
+async function requestPool(fns: () => Promise<any>, maxCount = 10) {
+  const set = new Set<Promise<any>>();
+  const promises: Promise<any>[] = [];
+  let index = 0;
+  while (index < fns.length) {
+    while (set.size < maxCount && index < fns.length) {
+      let fn = fns[index++]();
+      set.add(fn);
+      promises.push(fn);
+      fn.finally(() => {
+        set.delete(fn);
+      });
+    }
+    await Promise.race(Array.from(set));
+  }
+  return Promise.all(promises);
+}
+
+export { requestPool };
