0a65d5ca48e33d96c6371715c88c269e1567409e
wuliqun <540744846@qq.com>
2023-09-09 14:39:10 +0800

    add vue3 test

--- src/error-handle/pages/app.vue
+++ src/error-handle/pages/app.vue
@@ -3,10 +3,12 @@
     <error-vue />
     <button @click="nav('none')">nav to a unexist router</button><br />
     <button @click="nav('er')">nav to a router which throws error</button>
+    <router-link :to="{ name: 'er' }">{{ data.name }}</router-link>
+    <div @click="changeData()">change data</div>
   </div>
 </template>
 <script setup lang="ts">
-import { onErrorCaptured } from "vue";
+import { onErrorCaptured, reactive } from "vue";
 import { useRouter } from "vue-router";
 import ErrorVue from "./error.vue";
 
@@ -17,10 +19,20 @@ onErrorCaptured((err) => {
   console.log("app onErrorCaptured", err);
   // return false;
 });
+let count = 0;
+const data = reactive({
+  name: "wlq",
+});
 
 const router = useRouter();
 function nav(name) {
   router.push({ name });
+}
+function change(d) {
+  d.name = "xxxx";
+}
+function changeData() {
+  change(data);
 }
 </script>
 
--- src/error-handle/pages/error.vue
+++ src/error-handle/pages/error.vue
@@ -1,13 +1,43 @@
 <template>
   <div class="error">
     <button @click="testError">throw error</button>
+    <br />
+    <button @click="changeNum">changeNum</button>
+    <div ref="div">{{ doubleNum }}</div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { computed, ref, watch } from "vue";
+
 function testError() {
   throw "test";
 }
+
+// vue3
+const num = ref(0);
+const watchVal = ref(0);
+const div = ref<HTMLDivElement>();
+function changeNum() {
+  Promise.resolve(0).then(() => {
+    console.log("promise");
+  });
+  num.value += 1;
+  console.log("sync computed", doubleNum.value);
+  console.log("sync watch", watchVal.value);
+}
+const doubleNum = computed(() => {
+  console.log("computed", num.value * 2);
+  return num.value * 2;
+});
+
+watch(num, (val) => {
+  watchVal.value += 2;
+  console.log("double", doubleNum.value);
+  console.log("dom", div.value?.innerText);
+
+  console.log("watch", watchVal.value);
+});
 </script>
 
 <style lang="scss" scoped></style>
